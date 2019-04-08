0c9407000c6fa2716dfa5cf1e2bab88cffb162e8
Garry Tsaconas <gtsaconas@exchangesolutions.com>
2019-04-08 19:45:07 -0400

    Added a Loader to the Comments page.

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { RotateLoader } from "react-spinners";
 import getCommentsData from "../services/getCommentsData.service";
 
 interface Comments {
@@ -27,6 +28,8 @@ const Comments = props => {
   const [selectedPost, setSelectedPost] = useState<number | null>(null);
   const [isLoadingComments, setIsLoadingComments] = useState(true);
 
+  // TODO: Use a Suspense to prevent the "no-data" messag from showing on render.
+
   const goBack = () => {
     props.history.goBack();
   };
@@ -45,27 +48,39 @@ const Comments = props => {
   // Get the Original Post.
   const original_post = location.state.post.find(
     () => match.params.id === selectedPost
-  );
+  ) || { title: "", body: "" };
 
-  return selectedPost ? (
+  return (
     <div className="posts_comments">
-      <button onClick={goBack}>Go back</button>
-      <div className="posts_container --commentPost">
-        <h2 className="posts_container_title">{original_post.title}</h2>
-        <span className="posts_container_body">{original_post.body}</span>
-      </div>
-      {comments.map(comment => {
-        return (
-          <DisplayComment
-            key={`${comment.id}-${comment.postId}`}
-            {...comment}
-          />
-        );
-      })}
-      <button onClick={goBack}>Go back</button>
+      {isLoadingComments ? (
+        <div className="loader">
+          <RotateLoader color={"#123abc"} loading={true} />
+        </div>
+      ) : (
+        <>
+          {original_post.title !== "" && (
+            <>
+              <button onClick={goBack}>Go back</button>
+              <div className="posts_container --commentPost">
+                <h2 className="posts_container_title">{original_post.title}</h2>
+                <span className="posts_container_body">
+                  {original_post.body}
+                </span>
+              </div>
+            </>
+          )}
+          {comments.map(comment => {
+            return (
+              <DisplayComment
+                key={`${comment.id}-${comment.postId}`}
+                {...comment}
+              />
+            );
+          })}
+          <button onClick={goBack}>Go back</button>
+        </>
+      )}
     </div>
-  ) : (
-    <div>Sorry. This doesn't work.</div>
   );
 };
 
